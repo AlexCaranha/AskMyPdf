d8b28954ddbec2620f500250a161fa2d6af66711
Alex Caranha <alex.caranha@alcsoftwares.com>
2025-06-22 12:47:47 -0300

    main.py test with langserve [a test]

--- main.py
+++ main.py
@@ -1,93 +1,39 @@
-import os
-from langdetect import detect
-
-from dotenv import load_dotenv
-from src.pdf_loader import load_pdf, split_documents
-from src.qa_chain import build_qa_chain
-
-from src.translate import translate_to_english
-from src.vectorstore import create_vectorstore
-
-from src.util import Category, print_rich
+from fastapi import Depends, FastAPI, Header, HTTPException
+from langchain_core.runnables import RunnableLambda
+from typing_extensions import Annotated
+from langserve import add_routes
 
 
-load_dotenv()
+# Autenticação simples via header X-Token
+async def verify_token(x_token: Annotated[str, Header()] = "") -> None:
+    if x_token != "secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
 
 
-LLM_LOCAL_ENDPOINT: str = os.getenv("LLM_LOCAL_ENDPOINT")
-LLM_MODEL_NAME: str = os.getenv("LLM_MODEL_NAME")
-pdf_path: str = os.getenv("PDF_PATH")
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+app = FastAPI()
 
-SYSTEM_PROMPT = (
-    "Respond in a polite, concise, direct, and brief manner. "
-    "Provide only the information needed to answer the user's question, "
-    "avoiding lengthy explanations or unnecessary details. "
-    "After your answer, include a brief explanation to justify or clarify your response."
+
+# Exemplo de função simples para servir via langserve
+def make_a_question(question: str) -> str:
+    return question.upper()
+
+
+chain = RunnableLambda(make_a_question)
+
+
+add_routes(
+    app,
+    chain,
+    path="/AskMyPdf",
 )
 
 
-def text_or_audio_input():
-    while True:
-        user_input = input(
-            "\033[94mAsk something (or write 'mic' for microphone input): \033[0m"
-        )
-        user_input = user_input.strip().lower()
-
-        if user_input == "" or user_input == None:
-            continue
-
-        if user_input == "exit" or user_input == "quit":
-            return "exit"
-
-        # Detect if input is not English and translate
-        if user_input != "mic":
-            try:
-                lang = detect(user_input)
-            except Exception as e:
-                print_rich(category=Category.SYSTEM, text=e)
-                lang = "en"
-
-            if lang != "en":
-                translated = translate_to_english(user_input)
-                print_rich(category=Category.TRANSLATOR, text=translated)
-                continue
-
-        if user_input is not None:
-            break
-
-    return user_input
-
-
-def run():
-
-    print_rich(category=Category.ASKMYPDF, text=f"Loading PDF: {pdf_path}")
-    docs = load_pdf(pdf_path)
-
-    print_rich(category=Category.ASKMYPDF, text="Splitting into chunks")
-    chunks = split_documents(docs)
-
-    print_rich(category=Category.ASKMYPDF, text="Creating vector store")
-    vectorstore = create_vectorstore(chunks, LLM_LOCAL_ENDPOINT)
-
-    print_rich(category=Category.ASKMYPDF, text="Initializing chatbot")
-    qa_chain = build_qa_chain(vectorstore, LLM_MODEL_NAME, LLM_LOCAL_ENDPOINT)
-
-    print_rich(
-        category=Category.SYSTEM,
-        text="Type your question, or press Enter to use your microphone (say 'exit' or 'quit' to leave)",
-    )
-    while True:
-        user_input = text_or_audio_input()
-        if user_input == "exit" or user_input == "quit":
-            print_rich(category=Category.SYSTEM, text="AskMyPdf - Exiting")
-            break
-
-        full_prompt = f"{SYSTEM_PROMPT}\n\n{user_input}"
-        answer = qa_chain.invoke(full_prompt)
-        print_rich(category=Category.CHATBOT, text=answer["result"])
+@app.get("/")
+def root():
+    return {"message": "LangServe server is running!"}
 
 
 if __name__ == "__main__":
-    run()
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8002)
