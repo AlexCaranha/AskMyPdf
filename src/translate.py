d165528cda8a77dd3ef1ce980febdf14edd19f47
Alex Caranha <alex.caranha@alcsoftwares.com>
2025-06-23 02:36:54 -0300

    translate removed

--- src/translate.py
+++
@@ -1,8 +0,0 @@
-
-from deep_translator import GoogleTranslator
-
-
-def translate_to_english(text):
-    translator = GoogleTranslator(source="auto", target="en")
-    result = translator.translate(text)
-    return result
